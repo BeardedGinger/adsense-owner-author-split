8c67829d02dff92bd8490e12c33e34e43b5150fa
BeardedGinger <josh@limecuda.com>
2016-05-10 14:43:46 -0400

    initial commit / plugin header

---
+++ adsense-owner-author-split.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @since 		1.0.0
+ * @package 	Adsense_Owner_Author_Split
+ *
+ * Plugin Name: 	Adsense Owner/Author Split
+ * Plugin URI: 		https://github.com/BeardedGinger/adsense-owner-author-split
+ * Description: 	Allows for site owners and bloggers to split Adsense IDs used when displaying ads in blog posts
+ * Version: 		1.0.0
+ * Author: 			Josh Mallard
+ * Author URI:		http://joshmallard.com
+ * License:			GPL-2.0+
+ * License URI:		http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: 	adsense_owner_author_split
+ * Domain Path: 	/languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
