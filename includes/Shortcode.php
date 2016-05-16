12c943b61fa8553778334f8db913d3f34c59dfeb
BeardedGinger <josh@limecuda.com>
2016-05-16 08:49:12 -0400

    file setup for building shortcode

---
+++ includes/Shortcode.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Builds the shortcode for displaying ads in posts
+ *
+ * @since      1.0.0
+ * @package    adsense-owner-author-split
+ * @subpackage adsense-owner-author-split/includes
+ * @author     Josh Mallard <josh@limecuda.com>
+ */
+
+namespace GingerBeard\Adsense_Owner_Author_Split\Shortcode;
+
+class Shortcode {
+
+}
