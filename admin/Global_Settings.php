dc1fa01be6acd9cf2fde34a60cc0a4be7f3c48d1
BeardedGinger <josh@limecuda.com>
2016-09-07 00:48:05 -0400

    add the setting for controlling the inclusion of the shortcode by default within posts, update wording and labels for clarity #13

--- admin/Global_Settings.php
+++ admin/Global_Settings.php
@@ -116,7 +116,7 @@ class Global_Settings extends \Genesis_Admin_Boxes {
  	 */
 	function metaboxes() {
 
-		add_meta_box( 'aoas_default_settings', __( 'Defaults', 'owner_author_ad_split' ), array( $this, 'default_settings' ), $this->pagehook, 'main' );
+		add_meta_box( 'aoas_default_settings', __( 'General Settings', 'owner_author_ad_split' ), array( $this, 'default_settings' ), $this->pagehook, 'main' );
 		add_meta_box( 'aoas_above_ad_settings', __( 'Above-Content Ad', 'owner_author_ad_split' ), array( $this, 'above_ad' ), $this->pagehook, 'main' );
 		add_meta_box( 'aoas_shortcode_ad_settings', __( 'Content Ad', 'owner_author_ad_split' ), array( $this, 'shortcode_ad' ), $this->pagehook, 'main' );
 		add_meta_box( 'aoas_below_ad_settings', __( 'Below-Content Ad', 'owner_author_ad_split' ), array( $this, 'below_ad' ), $this->pagehook, 'main' );
@@ -132,10 +132,14 @@ class Global_Settings extends \Genesis_Admin_Boxes {
 
 		<table class="form-table">
 			<tbody>
+
 				<tr valign="top">
 					<th scope="row"></th>
 					<td>
-						<p class="description"><?php _e( 'By default ads show automatically on all posts. Alternatively, check this box and ads will be hidden by default and then can be enabled on a per-post basis.', 'owner_author_ad_split' ); ?></p>
+						<p>
+							<label for="<?php $this->field_id( 'hide_content_ads' ); ?>"><input type="checkbox" name="<?php $this->field_name( 'hide_content_ads' ); ?>" id="<?php $this->field_id( 'hide_content_ads' ); ?>" value="1" <?php checked( $this->get_field_value( 'hide_content_ads' ) ); ?> /><?php _e( 'Hide Content Ads by Default', 'owner-author-ad-split' ); ?></label>
+							<p class="description"><?php _e( 'By default ads show automatically on all posts. Alternatively, check this box and ads will be hidden by default and then can be enabled on a per-post basis.', 'owner-author-ad-split' ); ?></p>
+						</p>
 					</td>
 				</tr>
 
@@ -143,7 +147,7 @@ class Global_Settings extends \Genesis_Admin_Boxes {
 					<th scope="row"></th>
 					<td>
 						<p>
-							<label for="<?php $this->field_id( 'hide_content_ads' ); ?>"><input type="checkbox" name="<?php $this->field_name( 'hide_content_ads' ); ?>" id="<?php $this->field_id( 'hide_content_ads' ); ?>" value="1" <?php checked( $this->get_field_value( 'hide_content_ads' ) ); ?> /><?php _e( 'Hide Content Ads by Default', 'owner_author_ad_split' ); ?></label>
+							<label for="<?php $this->field_id( 'default_content_shortcode' ); ?>"><input type="checkbox" name="<?php $this->field_name( 'default_content_shortcode' ); ?>" id="<?php $this->field_id( 'default_content_shortcode' ); ?>" value="1" <?php checked( $this->get_field_value( 'default_content_shortcode' ) ); ?> /><?php _e( 'Include the <code>[gb_ad]</code> shortcode within the WYSIWYG by default when drafting a new post', 'owner-author-ad-split' ); ?></label>
 						</p>
 					</td>
 				</tr>
@@ -181,7 +185,7 @@ class Global_Settings extends \Genesis_Admin_Boxes {
 					<th scope="row"><label for="owner_above_weight"><?php _e( 'Weight', 'owner_author_ad_split' ); ?></label></th>
 					<td>
 						<p><input type="range" min="0" max="10" step="1" name="<?php echo $this->get_field_name( 'owner_above_weight' ); ?>" class="regular-text" id="owner_above_weight" value="<?php echo esc_attr( $this->get_field_value( 'owner_above_weight' ) ); ?>" oninput="outputUpdate('#owner_above_weight_output', value)" /></p>
-						<p class="description">Show the Owner's ad above the content for every <strong><output for="owner_above_weight" id="owner_above_weight_output"><?php echo esc_attr( $this->get_field_value( 'owner_above_weight' ) ); ?></output></strong> out of 10 page loads</p>
+						<p class="description">Show the Owner's ad above the content on every <strong><output for="owner_above_weight" id="owner_above_weight_output"><?php echo esc_attr( $this->get_field_value( 'owner_above_weight' ) ); ?></output></strong> out of 10 page loads</p>
 					</td>
 				</tr>
 
@@ -216,7 +220,7 @@ class Global_Settings extends \Genesis_Admin_Boxes {
 					<th scope="row"><label for="owner_shortcode_weight"><?php _e( 'Weight', 'owner_author_ad_split' ); ?></label></th>
 					<td>
 						<p><input type="range" min="0" max="10" step="1" name="<?php echo $this->get_field_name( 'owner_shortcode_weight' ); ?>" class="regular-text" id="owner_shortcode_weight" value="<?php echo esc_attr( $this->get_field_value( 'owner_shortcode_weight' ) ); ?>" oninput="outputUpdate('#owner_shortcode_weight_output', value)" /></p>
-						<p class="description">Show the Owner's ad in the shortcode for every <strong><output for="owner_shortcode_weight" id="owner_shortcode_weight_output"><?php echo esc_attr( $this->get_field_value( 'owner_shortcode_weight' ) ); ?></output></strong> out of 10 page loads</p>
+						<p class="description">Show the Owner's ad in the shortcode on every <strong><output for="owner_shortcode_weight" id="owner_shortcode_weight_output"><?php echo esc_attr( $this->get_field_value( 'owner_shortcode_weight' ) ); ?></output></strong> out of 10 page loads</p>
 					</td>
 				</tr>
 
@@ -255,7 +259,7 @@ class Global_Settings extends \Genesis_Admin_Boxes {
 					<th scope="row"><label for="owner_below_weight"><?php _e( 'Weight', 'owner_author_ad_split' ); ?></label></th>
 					<td>
 						<p><input type="range" min="0" max="10" step="1" name="<?php echo $this->get_field_name( 'owner_below_weight' ); ?>" class="regular-text" id="owner_below_weight" value="<?php echo esc_attr( $this->get_field_value( 'owner_below_weight' ) ); ?>" oninput="outputUpdate('#owner_below_weight_output', value)" /></p>
-						<p class="description">Show the Owner's ad below the content for every <strong><output for="owner_below_weight" id="owner_below_weight_output"><?php echo esc_attr( $this->get_field_value( 'owner_below_weight' ) ); ?></output></strong> out of 10 page loads</p>
+						<p class="description">Show the Owner's ad below the content on every <strong><output for="owner_below_weight" id="owner_below_weight_output"><?php echo esc_attr( $this->get_field_value( 'owner_below_weight' ) ); ?></output></strong> out of 10 page loads</p>
 					</td>
 				</tr>
 
